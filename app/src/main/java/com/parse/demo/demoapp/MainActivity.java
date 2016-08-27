ea275168519d1e9686ca8c21fb6329424a6d1e80
luyen dat <dat@netobjex.com>
2016-08-27 10:03:28 +0700

    update code insert data

--- app/src/main/java/com/parse/demo/demoapp/MainActivity.java
+++ app/src/main/java/com/parse/demo/demoapp/MainActivity.java
@@ -9,6 +9,8 @@ import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
     public static final String APPLICATION_ID = "SGeYuXVlPid56Obae9nz3fB75kRIV7gfzByRIPdn";
     public static final String CLIENT_KEY = "Lj6KbsPotHTU9aUGyfcA6W2024MCldTngM42DZtx";
     public static final String MASTER_KEY = "DhJOlV4JU49MCHzglJsBjrjxlmLCiH50leM4rFDc";
-
 
 
     @Override
@@ -52,6 +53,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        // Insert data
+        final ParseObject ocarina = new ParseObject("Coupons");
+
+        // ParseObjects are just property bags, so we get no assurances that we're putting
+        // the right kinds of objects in our Instrument.
+        ocarina.put("codeString", "xxx-yyy");
+        ocarina.put("couponType", 1);
+        ocarina.put("discountType", 1);
+        ocarina.put("discountValue", "100");
+        ocarina.saveInBackground(new SaveCallback() {
+            public void done(ParseException e) {
+                // We have to use utility classes to add behavior to our ParseObjects.
+                if (e != null)
+                    System.out.println("Insert data successed");
+                else
+                    System.out.print(" ------- Messages recived " + e.getMessage());
+            }
+        });
 
 
     }
